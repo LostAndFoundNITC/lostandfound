b13cc81d9bf330328e77ebeafab85b2e2776a038
Aniketsid <aniketsid043@gmail.com>
2017-03-10 10:50:33 +0600

    with login enabled

--- app/src/main/java/com/nsl/lostandfound/MainActivity.java
+++ app/src/main/java/com/nsl/lostandfound/MainActivity.java
@@ -1,11 +1,34 @@
 package com.nsl.lostandfound;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
 
 /**
  * A login screen that offers login via email/password.
@@ -18,46 +41,141 @@ public class MainActivity extends AppCompatActivity{
 
    // public static final String EMAIL = "com.nsl.lostandfound.MAIL";
    // public static final String PASSWORD = "com.nsl.lostandfound.PASS";
-
+            Context context;
+    EditText UsernameEt, PasswordEt;
+    String result="";
+    MainActivity x =this;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        UsernameEt = (EditText)findViewById(R.id.input_email);
+        PasswordEt = (EditText)findViewById(R.id.input_password);
+        EditText e =(EditText) findViewById(R.id.input_email);
+        e.clearComposingText();
+        EditText p =(EditText) findViewById(R.id.input_password);
+        p.clearComposingText();
     }
-
-
-
-
-    private boolean isEmailValid(String email) {
+ private boolean isEmailValid(String email) {
         //TODO: Replace this with your own logic
         return email.contains("@");
     }
-
-    private boolean isPasswordValid(String password) {
+ private boolean isPasswordValid(String password) {
         //TODO: Replace this with your own logic
         return password.length() > 4;
     }
 
 
     public void login(View view) {
-        Intent intent = new Intent(this, home.class);
-        EditText email = (EditText) findViewById(R.id.input_email);
-        String mail = email.getText().toString();
-        EditText password = (EditText) findViewById(R.id.input_password);
-        String pass = password.getText().toString();
+        String username = UsernameEt.getText().toString();
+        String password = PasswordEt.getText().toString();
 
-       // intent.putExtra(EMAIL, mail);
-      // intent.putExtra(PASSWORD, pass);
-       if(isEmailValid(mail) && isPasswordValid(pass))
+      if(isEmailValid(username) && isPasswordValid(password))
         {
-            startActivity(intent);
+            String type = "login";
+            new BackgroundWorker(this).execute(type, username.trim(), password.trim());
 
-        }
+
+   }
         else {
-            TextView tView = (TextView) findViewById(R.id.warning);
-            tView.setText("Invalid Credentials");
-        }
+             TextView t =(TextView)findViewById(R.id.warning);
+          t.clearComposingText();
+          t.setText("Invalid Pattern");
+          }
+
     }
 
 
+
+
+
+
+
+
+
+    class BackgroundWorker extends AsyncTask<String,Void,String> {
+        //Context context;
+
+        public char res;
+        AlertDialog alertDialog;
+        BackgroundWorker (Context ctx) {
+            context = ctx;
+        }
+        @Override
+        protected String doInBackground(String... params) {
+            String type = params[0];
+            String login_url = "http://andromeda.nitc.ac.in/~m150035ca/login.php";
+            if(type.equals("login")) {
+                try {
+                    String user_name = params[1];
+                    String password = params[2];
+                    URL url = new URL(login_url);
+                    HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
+                    httpURLConnection.setRequestMethod("POST");
+                    httpURLConnection.setDoOutput(true);
+                    httpURLConnection.setDoInput(true);
+                    OutputStream outputStream = httpURLConnection.getOutputStream();
+                    BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+                    String post_data = URLEncoder.encode("user_name","UTF-8")+"="+URLEncoder.encode(user_name,"UTF-8")+"&"
+                            +URLEncoder.encode("password","UTF-8")+"="+URLEncoder.encode(password,"UTF-8");
+                    bufferedWriter.write(post_data);
+                    bufferedWriter.flush();
+                    bufferedWriter.close();
+                    outputStream.close();
+                    InputStream inputStream= httpURLConnection.getInputStream();
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream,"iso-8859-1"));
+
+                    String line="";
+                    while((line = bufferedReader.readLine())!= null) {
+                        result += line;
+                    }
+                    res=result.toCharArray()[result.length()-1];
+
+                    bufferedReader.close();
+
+                    inputStream.close();
+                    httpURLConnection.disconnect();
+                    return result;
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            alertDialog = new AlertDialog.Builder(context).create();
+            alertDialog.setTitle("Login Status");
+        }
+
+
+        @Override
+        protected void onPostExecute(String result) {
+
+            TextView t =(TextView)findViewById(R.id.warning);
+            t.clearComposingText();
+            //alertDialog.setMessage(res);
+            //alertDialog.show();
+            if(res == 't')
+            {
+                Toast.makeText(context,"Logged In Successfully !!",Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(x,home.class);
+                startActivity(intent);
+
+            }
+            else {
+
+
+                Toast.makeText(context," Unsuccessful Login !!",Toast.LENGTH_LONG).show();
+            }
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            super.onProgressUpdate(values);
+        }
+    }
 }
