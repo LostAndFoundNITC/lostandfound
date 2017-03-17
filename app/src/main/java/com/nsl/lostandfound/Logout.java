4bb83fc49c97534cab4c8bbf190ca73b5c4c7416
lakshitgarg <garg_lakshit@yahoo.com>
2017-03-17 18:21:53 +0530

    Added Logout files

---
+++ app/src/main/java/com/nsl/lostandfound/Logout.java
@@ -0,0 +1,14 @@
+package com.nsl.lostandfound;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+
+/**
+ * Created by lakshit on 3/17/2017.
+ */
+
+public class Logout extends MainActivity {
+
+
+}
