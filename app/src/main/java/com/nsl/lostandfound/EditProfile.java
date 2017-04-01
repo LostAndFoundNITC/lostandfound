a66224aeaace24c5ff2e1652692bf21b8f7e5632
lakshitgarg <garg_lakshit@yahoo.com>
2017-04-01 11:34:19 +0530

    Doxygen comments added

--- app/src/main/java/com/nsl/lostandfound/EditProfile.java
+++ app/src/main/java/com/nsl/lostandfound/EditProfile.java
@@ -15,14 +15,19 @@ import android.widget.Toast;
 */
 public class EditProfile extends ActionBarActivity {
     EditText AddressEt, PhoneEt;
-    @Override
+     /**\fn onCreate function.
+	*\brief Fetched the value of parameters address and phone from updateprofile acivity.
+	*/
+        @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
         AddressEt = (EditText)findViewById(R.id.etAddress);
         PhoneEt = (EditText)findViewById(R.id.etPhone);
     }
-
+ /**\fn UpdateProfile function.
+	*\brief Call the BackGroundWorkerProfile class and sends address, contact and email to the class.
+	*/
     public void UpdateProfile(View view) {
         Bundle extras = getIntent().getExtras();
         String address = AddressEt.getText().toString();
