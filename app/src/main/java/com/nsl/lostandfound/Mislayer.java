b8d4f42bee244d4aedea00162bd1db48c938c727
sooryae <soorya_m150035ca@nitc.ac.in>
2017-04-01 12:00:40 +0530

    Doxygen comments added

--- app/src/main/java/com/nsl/lostandfound/Mislayer.java
+++ app/src/main/java/com/nsl/lostandfound/Mislayer.java
@@ -72,8 +72,7 @@ public class Mislayer extends MainActivity
     public boolean onOptionsItemSelected(MenuItem item) {
         /**
         *Handle action bar item clicks here. The action bar will
-        *automatically handle clicks on the Home/Up button, so long
-        *as you specify a parent activity in AndroidManifest.xml.
+        *automatically handle clicks on the Home/Up button, so long as you specify a parent activity in AndroidManifest.xml.
         */
         int id = item.getItemId();
 
