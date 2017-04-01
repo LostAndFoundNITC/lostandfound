eb8e75ab26297cbe16bf505093e281c657153501
Aniketsid <aniketsid043@gmail.com>
2017-04-01 10:08:02 +0530

    Doxygen Comments added

--- app/src/main/java/com/nsl/lostandfound/MyPost.java
+++ app/src/main/java/com/nsl/lostandfound/MyPost.java
@@ -1,5 +1,12 @@
-package com.nsl.lostandfound;
+/**
+*\brief This is the MyPost file.This Code will be invoked when 'My Posts' button is pressed
+*It is in the package com.nsl.lostandfound;
+*/
 
+package com.nsl.lostandfound;
+/**
+*Below are all imported classes/interfaces.
+*/
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
@@ -18,10 +25,16 @@ import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.common.api.Status;
 
 import static android.R.attr.name;
-
+/**\class class with name MyPost is being created here.Mypost extends AppCompatActivity NavigationView.OnNavigationItemSelectedListener
+    inorder to be an subclass of the class Activity and NavigationView.
+*/
 public class MyPost extends MainActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+
+    /**\fn onCreate function...
+	*\brief Shows the activity on the screen on which all the posts of the current user are shown.
+	*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,17 +52,27 @@ public class MyPost extends MainActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
         Bundle extras = getIntent().getExtras();
+         /**
+	*Email id of the currrent user is fetched.
+	*/
         String email = extras.getString("email");
 
         WebView w;
         w = (WebView) findViewById(R.id.MyPost);
         w.setWebViewClient(new WebViewClient());
-
+        /**
+	*Below condition detects if internet is active on the device
+	*/
         if (!DetectConnection.checkInternetConnection(this)) {
+                 /**
+                            *If internet is not active ,'No Internet' message is shown
+                            */
             Toast.makeText(getApplicationContext(), "No Internet!", Toast.LENGTH_LONG).show();
 
         } else {
-
+                         /**
+                    *If internet is working ,All the claims of current user are shown.
+                    */
             w.clearCache(true);
             w.clearHistory();
             w.loadUrl("http://andromeda.nitc.ac.in/~m150035ca/Web/mypost.php?email='" + email + "'");
@@ -58,7 +81,9 @@ public class MyPost extends MainActivity
 
 
     }
-
+/**\fn onBackPressed function
+	*\brief Handles the action when back button is pressed i.e. redirects to Dashboard(Home Activity)
+	*/
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -73,21 +98,25 @@ public class MyPost extends MainActivity
             finish();
         }
     }
-
+                /**\fn onCreateOptionsMenu function
+	*\brief Handles the action when  a perticular option from navigation drawer is selected.
+	*/
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+     /**
+                    * Inflate the menu; this adds items to the action bar if it is present.
+                    */
+       
         getMenuInflater().inflate(R.menu.my_post, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+        
         int id = item.getItemId();
 
+          
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
@@ -99,19 +128,23 @@ String email;
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
+          /**
+                    *  Handle navigation view item clicks here.
+                    */
+       
         int id = item.getItemId();
         Bundle extras = getIntent().getExtras();
         String name= extras.getString("name");
         email= extras.getString("email");
-        // String photo = extras.getString("photo");
         if (id == R.id.lost) {
             Intent intent = new Intent(this, Mislayer.class);
-            // intent.putExtra("photo",photo);
             intent.putExtra("name",name);
             intent.putExtra("email",email);
             startActivity(intent);
-            // Handle the lost action
+               /**
+                    * Handle the lost action
+                    */
+    
         }else if (id == R.id.home) {
             Intent intent = new Intent(this, home.class);
             //intent.putExtra("photo",photo);
@@ -119,23 +152,38 @@ String email;
             intent.putExtra("email",email);
             startActivity(intent);
         }
+          /**
+                    * Handle the found action
+                    */
         else if (id == R.id.found) {
             Intent intent = new Intent(this, Finder.class);
             intent.putExtra("name",name);
             intent.putExtra("email",email);
             startActivity(intent);
-        } else if (id == R.id.user_posts) {
+        } 
+            /**
+                    * Handle the My Posts action
+                    */
+        else if (id == R.id.user_posts) {
             Intent intent = new Intent(this, MyPost.class);
             intent.putExtra("name",name);
             intent.putExtra("email",email);
             startActivity(intent);
-        } else if (id == R.id.user_profile) {
+        } 
+          /**
+                    * Handle the Profile action
+                    */
+        else if (id == R.id.user_profile) {
             Intent intent = new Intent(this, UserProfile.class);
             //intent.putExtra("photo",photo);
             intent.putExtra("name",name);
             intent.putExtra("email",email);
             startActivity(intent);
-        } else if (id == R.id.logout) {
+        } 
+          /**
+                    * Handle the Logout action
+                    */
+        else if (id == R.id.logout) {
             signOut();
             revokeAccess();
 
@@ -145,7 +193,9 @@ String email;
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
-
+      /**\fn signOut function
+	*\brief Handles the action when Logout from navigation drawer is selected.
+	*/
     private void signOut() {
         Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(
                 new ResultCallback<Status>() {
