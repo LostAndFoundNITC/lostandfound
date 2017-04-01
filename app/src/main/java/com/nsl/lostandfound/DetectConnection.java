67dbc50e0186f37828802b836f44596ada1cc039
Aniketsid <aniketsid043@gmail.com>
2017-04-01 09:43:51 +0530

    Doxygen Comments added

--- app/src/main/java/com/nsl/lostandfound/DetectConnection.java
+++ app/src/main/java/com/nsl/lostandfound/DetectConnection.java
@@ -1,10 +1,22 @@
-package com.nsl.lostandfound;
 
+/**
+*\brief This is the DetectConnection file.This contains the function that detects if internet is active or not.
+*It is in the package com.nsl.lostandfound;
+*/
+
+package com.nsl.lostandfound;
+/**
+*Below are all imported classes/interfaces.
+*/
 import android.content.Context;
 import android.net.ConnectivityManager;
 
-
+/**\class class with name DetectConnection is being created here.
+*/
 public class DetectConnection {
+        /**\fn checkInternetConnection function
+	*\brief Return true;If Internet is active else false;
+	*/
     public static boolean checkInternetConnection(Context context) {
 
         ConnectivityManager con_manager = (ConnectivityManager)
